7df9d8f12f6fd5985ac649af778b00bf3c169efd
bharathviswa2612 <bharathviswa2612@gmail.com>
2025-12-17 12:39:26 +0530

    Add Spring Boot base project for IMS

---
+++ src/test/java/com/ims/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
